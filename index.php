2efe1f7c1132fe2f53423e8845778bf35a4ba0f6
Yevhen Korniievskyi <y.korniievskyi@dev-branch.com>
2023-05-01 23:32:35 +0300

    Studuing arrays.

--- index.php
+++ index.php
@@ -357,7 +357,7 @@
         function setHeight(int $minheight=50) {
             if ($minheight < 50) {
                 echo "Height is $minheight. The minimal Height must be equal of 50.<br>";
-                // break; // Doesn't work here becouse in doesn't switch or while statement.
+                // break; // Doesn't work here because in doesn't switch or while statement.
             } else {
             echo "The height is: $minheight.<br>";
             }
@@ -391,6 +391,48 @@
         $num = 2;
         add_five($num);
         echo $num;
+        ?>
+
+        <h2>Arrays</h2>
+        <?php
+        $cars = array("BMW", "Toyota", "Volvo");
+        echo "Here we have " . $cars[0] . ", " . $cars[1] . ", and " . $cars[2] . ".";
+        br();
+
+        $arrlength = count($cars);
+        for ($x=0; $x< $arrlength; $x++) {
+            echo $cars[$x];
+            echo "<br>";
+        }
+
+        $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+        echo "Peter is " . $age['Peter'] . " years old.";
+
+        br();
+        foreach($age as $name => $years) {
+            echo $name . " is " . $years . " years old.";
+            echo "<br>";
+        }
+
+        $cars = array(
+            array("Volvo",22,18),
+            array("BMW",15,13),
+            array("Saab",5,2),
+            array("Land Rover",17,15)
+        );
+        echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2]."<br>";
+        echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2]."<br>";
+        echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2]."<br>";
+        echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2]."<br>";
+
+        for ($row = 0; $row < 4; $row++) {
+            echo "<p><b>Row number $row</b></p>";
+            echo "<ul>";
+            for ($col = 0; $col < 3; $col++) {
+                echo "<li>".$cars[$row][$col]."</li>";
+            }
+            echo "</ul>";
+        }
         ?>
     </body>
 </html>
